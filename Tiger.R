b38771240fd2cabf366502637b4f49dfbd5b3e8c
Ananya Kapur <ank179@pitt.edu>
2019-10-25 15:09:16 -0400

    added table and figure

--- Tiger.R
+++ Tiger.R
@@ -2,3 +2,9 @@
 
 tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
 head(tigerData)
+
+tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
+tigerTable
+data.frame(Frequency = tigerTable)
+data.frame(Frequency = addmargins(tigerTable))
+barplot(tigerTable, ylab = "Frequency", cex.names = 0.5, las = 2)
